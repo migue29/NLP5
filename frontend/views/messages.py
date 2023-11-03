a9d7e2aa69dbae2d827724c640910639a78d6993
ProKav <kamilo_vanegas@hotmail.com>
2023-11-03 17:20:44 +0100

    feat: Create messages and text from variables that are rendered in App

---
+++ frontend/views/messages.py
@@ -0,0 +1,15 @@
+TITLE = "Hate Guard"
+
+INTRO = """
+    Bienvenido a Hate Guard, una herramienta impulsada por inteligencia artificial para detectar y combatir el discurso de odio en línea. Nuestra misión es fomentar un ambiente digital inclusivo y amigable para todos.
+    """
+
+BAR = "Menu"
+
+CYBORG = "./assets/images/guardian.png"
+
+AUTHORS = """
+    <div class="footer">
+        <p>Made with ❤️‍🔥 by <a href="https://www.linkedin.com/in/javi-navarro-data-analyst/" target="_blank">Javier</a> / <a href="https://www.linkedin.com/in/alexa-montenegro-047b3a252/" target="_blank">Alexa</a> / <a href="https://www.linkedin.com/in/miguelmendozaespinoza9a010114a/" target="_blank">Miguel</a> / <a href="https://www.linkedin.com/in/kamilodev/" target="_blank">Camilo</a></p>
+    </div>
+    """
