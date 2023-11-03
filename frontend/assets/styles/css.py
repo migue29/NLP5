edc56d4be1339af69d68cec8cb9c80cd6e054926
ProKav <kamilo_vanegas@hotmail.com>
2023-11-03 17:16:15 +0100

    feat: Create styles for streamlit dashboard

---
+++ frontend/assets/styles/css.py
@@ -0,0 +1,76 @@
+styles = """
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Black+Ops+One&family=Exo:wght@400;700&family=Handlee&family=Lato&display=swap');
+    
+[data-testid="stAppViewContainer"] {
+    background-image: linear-gradient(
+    220deg,
+    hsl(240deg 15% 80%) 0%,
+    hsl(219deg 24% 79%) 26%,
+    hsl(207deg 31% 77%) 43%,
+    hsl(198deg 34% 75%) 57%,
+    hsl(189deg 33% 72%) 69%,
+    hsl(179deg 29% 70%) 78%,
+    hsl(165deg 28% 70%) 86%,
+    hsl(145deg 25% 71%) 92%,
+    hsl(110deg 21% 71%) 97%,
+    hsl(77deg 23% 68%) 100%
+);
+}
+
+footer {visibility: hidden;}
+
+.footer {
+        width: 50%;
+        text-align-last: center;
+        padding: 1rem;
+        bottom: 0;
+        position: fixed;
+    }
+
+p {
+    font-size: 1.3rem;
+    font-family: 'Handlee', cursive;
+    text-align: justify;
+}
+
+.footer p {
+    font-family: 'Lato', sans-serif;
+    font-size: 0.8rem;
+    color: #FF0100;
+}
+
+.footer a {
+    text-decoration: none;
+    color: #FF0100;
+    font-weight: bold;
+    padding: 0 0.2rem;
+}
+
+h1 {
+    font-family: 'Black Ops One', cursive;
+    font-size: 4em;
+    text-align: center;
+    text-shadow: 3px 3px 3px #000000;
+}
+
+h2 {
+    font-family: 'Exo', sans-serif;
+    font-size: 2em;
+}
+
+h3 {
+    font-family: 'Exo', sans-serif;
+    font-size: 2em;
+    text-align: center;
+}
+
+img {
+    width: 23%;
+    position: fixed;
+    z-index: 10;
+    align-self: flex-end;
+    padding-right: 5%;
+}
+</style>
+"""
