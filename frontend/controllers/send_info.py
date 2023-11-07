5f70644befc239225fada0be9ae82ad7f84dda27
Miguel <miguel.almespinoza@gmail.com>
2023-11-07 13:09:13 +0100

    Feat: we established the first connection between fastapi and streamlit

--- frontend/controllers/send_info.py
+++ frontend/controllers/send_info.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import requests
 
 def send_info(topic, mood):
-    st.write(topic, mood)
+    # st.write(topic, mood)
+    try:
+        response = requests.get("http://localhost:8000/predict/message")
+
+    except Exception as error:
+        st.error(f"Ha ocurrido un error: {error}")
