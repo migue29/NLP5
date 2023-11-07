f721eb8a8fa53c33f15752a367d360de715cf2d9
Miguel <miguel.almespinoza@gmail.com>
2023-11-07 10:56:58 +0100

    Feat: Post type endpoint is added

--- backend/controllers/hate_predictor.py
+++ backend/controllers/hate_predictor.py
@@ -7,10 +7,24 @@ async def receive_message(response:Response):
     """
     try:
         return {
-            "message": "No funcionó"
+            "message": "Todo es correcto"
             }
         response.status_code = status.HTTP_200_OK
     except Exception as error:
         return{
             "message": f"No se pudo realizar la solicitud, {error}"
+        }
+
+async def make_prediction(text: str, response:Response):
+    """
+    Realizamos la predicción
+    """
+    try:
+        print(text)
+        return True
+        response.status_code = status.HTTP_200_OK
+
+    except Exception as error:
+        return{
+            "message": f"Hubo un problema, {error}"
         }
