b6e5f460b22a2a3012e07dec50b728d86732ea09
Miguel <miguel.almespinoza@gmail.com>
2023-11-13 15:13:16 +0100

    Refactor: extract topic and mood functions from the send_info file

--- frontend/controllers/send_info.py
+++ frontend/controllers/send_info.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import requests
+from controllers import send_text
+from controllers import send_mood
 
 def send_info(topic, mood):
-    # st.write(topic, mood)
     try:
-        response = requests.get("http://localhost:8000/predict/message")
+        send_text.send_text(topic)
+        send_mood.send_mood(mood)
 
     except Exception as error:
         st.error(f"Ha ocurrido un error: {error}")
