e662595400ec24ea73aa80a2e3ed0c1a2738a918
Miguel <miguel.almespinoza@gmail.com>
2023-11-21 14:25:31 +0100

    feat: start path is added to the frontend for powerbi

--- frontend/pages/02_📈_Analisis.py
+++ frontend/pages/02_📈_Analisis.py
@@ -2,15 +2,20 @@ import streamlit as st
 from assets.styles.css import styles
 from views.messages import AUTHORS
 
-def analisis():
-    st.set_page_config(
-        page_title='Analisis',
-        page_icon='📈'
+#def analisis():
+st.set_page_config(
+    page_title='Analisis',
+    page_icon='📈'
     )
     
-    st.markdown(styles, unsafe_allow_html=True)
-    st.title("📈 Analisis")
-    st.markdown(AUTHORS, unsafe_allow_html=True)
+st.markdown(styles, unsafe_allow_html=True)
+st.title("📈 Analisis")
+st.markdown(AUTHORS, unsafe_allow_html=True)
     
-if __name__ == "__main__":
-    analisis()
+codigo_insercion_powerbi ="""
+    <iframe title="analisis_nlp1" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=3fe38db6-3acd-4192-a441-c3e504a40376&ctid=7d979bda-6909-4e84-9f48-1674dc104477" frameborder="0" allowFullScreen="true"></iframe>
+"""
+st.markdown(codigo_insercion_powerbi, unsafe_allow_html=True)
+
+__name__ == "__main__"
+    #analisis()
